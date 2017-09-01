1d80223387f7eb8f533f7fd4349f7783b67ae1f9
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-01 15:42:02 +0200

    Added loading animation

--- src/loader.js
+++ src/loader.js
@@ -2,9 +2,44 @@ const {webContents} = require('electron');
 const { app, BrowserWindow } = require("electron");
 window.$ = window.jQuery = require('jquery');
 
+var _r = true;
+
 window.onload = () => {
-	//Tasks
 	$(document).ready(function() {
-		setTimeout(function(){ window.location = "../layouts/main.html"; }, 3000);
+		var loader = new preloader("Loading ", ["◡","◟","◜","◠","◝","◞"]);
+		loader.start();
+		var _c = 4;
+		var _dot = "";
+		setInterval(function(){
+			_c--;
+			_dot += ".";
+			loader.message("Loading" + _dot + " ");
+			if (_c === 0){
+				loader.stop();
+				clearInterval(this);
+				next();
+			}
+		}, 500);
 	});
+}
+
+function next(){ if(_r) window.location = "../layouts/main.html"; }
+
+function preloader(s_msg, s_style){
+	this.start = function(){
+		var _ = this;
+		function startAnim(arr) {
+			var len = arr.length, i = 0, interval = 90;
+			var drawTick = function(){
+				var _s = arr[i++ % len];
+				$(".l_ico").text(_s);
+				$(".l_txt").text(s_msg);
+			};
+			_.timer = setInterval(drawTick, interval);
+		}
+		var frames = s_style.map(function(c){ return c; });
+		startAnim(frames, 70);
+	};
+	this.message = function(msg){ s_msg = msg; };
+	this.stop = function(){ clearInterval(this.timer); };
 }
