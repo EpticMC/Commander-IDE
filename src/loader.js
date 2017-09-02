bbff4d3caf0625a2c1d6a313f4ff99b7368469f3
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-02 23:25:26 +0200

    Improved delay

--- src/loader.js
+++ src/loader.js
@@ -6,24 +6,14 @@ var _r = true;
 
 window.onload = () => {
 	$(document).ready(function() {
-		var loader = new preloader("Loading ", ["◡","◟","◜","◠","◝","◞"]);
-		loader.start();
-		var _c = 4;
-		var _dot = "";
-		setInterval(function(){
-			_c--;
-			_dot += ".";
-			loader.message("Loading" + _dot + " ");
-			if (_c === 0){
-				loader.stop();
-				clearInterval(this);
-				next();
-			}
-		}, 500);
+		setTimeout(startLoader(), 500);
+		return;
 	});
 }
 
-function next(){ if(_r) window.location = "../layouts/main.html"; }
+function next(){ setTimeout(end(), 500); }
+
+function end(){ if(_r) window.location = "../layouts/main.html"; }
 
 function preloader(s_msg, s_style){
 	this.start = function(){
@@ -42,4 +32,21 @@ function preloader(s_msg, s_style){
 	};
 	this.message = function(msg){ s_msg = msg; };
 	this.stop = function(){ clearInterval(this.timer); };
+}
+
+function startLoader(){
+	var loader = new preloader("Loading ", ["◡","◟","◜","◠","◝","◞"]);
+	loader.start();
+	var _c = 4;
+	var _dot = "";
+	setInterval(function(){
+		_c--;
+		_dot += ".";
+		loader.message("Loading" + _dot + " ");
+		if (_c === 0){
+			loader.stop();
+			clearInterval(this);
+			next();
+		}
+	}, 500);
 }
