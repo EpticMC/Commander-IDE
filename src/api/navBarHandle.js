7f2b478c66194aa3e2196a80a9a21718b5e573a8
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-03 03:00:57 +0200

    Added navbar handle

--- src/api/navBarHandle.js
+++ src/api/navBarHandle.js
@@ -3,5 +3,16 @@ const { app, BrowserWindow } = require("electron");
 window.$ = window.jQuery = require("jquery");
 
 $(document).ready(function() {
+	$(".nav-bar ul li").click(function(e) {
+		e.stopPropagation();
+		$(".visible").removeClass("visible");
+		$(this).children(" div.submenu").addClass("visible").fadeIn();
+		$(".selected").removeClass("selected");
+		$(this).addClass("selected");
+	});
 
+	$("body:not(.nav-bar ul.left li), body:not(.nav-bar li input#search)").click(function() {
+		$(".selected").removeClass("selected");
+		$(".visible").removeClass("visible");
+	});
 });
