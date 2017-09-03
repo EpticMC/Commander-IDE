58dcb0e163cabbcffb5ed8adf6fd4657faaf29b2
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-03 03:00:44 +0200

    Moved file manager to class

--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
 const { ipcRenderer, remote } = require("electron");
 const loader = require("monaco-loader");
 const fs = require("fs");
-
-
-function fileManager({editor}) {
-    this.editor = editor
-    document.querySelector("#open").onclick = () => openFile();
-    document.querySelector("#save").onclick = () => saveFile();
-
-    function openFile() {
-        var url = remote.dialog.showOpenDialog();
-        fs.readFile(url[0], "utf-8", (err, data) => { this.editor.setModel(monaco.editor.createModel(data, "javascript")); });
-    }
-
-    function saveFile() {
-        remote.dialog.showSaveDialog(filename => {
-            let data = "";
-            let model = this.editor.getModel();
-            model._lines.forEach(line => data += line.text + model._EOL);
-            fs.writeFile(filename, data, "utf-8");
-        });
-    }
-}
+const FileManager = require("./api/fileManager");
 
 window.onload = () => {
     loader().then((monaco) => {
@@ -31,10 +11,15 @@ window.onload = () => {
             cursorBlinking: "smooth",
             cursorStyle: "line"
         });
-        fileManager({editor});
+
+        let fileManager = new FileManager({editor});
+
         ipcRenderer.on("navigate", (e, url) => {
             url = url.slice(8);
             fs.readFile(url, "utf8", (error, result) => { if(!error) editor.setModel(monaco.editor.createModel(result, "javascript")); });
         });
+
+        $("#f__open").click(function() { fileManager.openFile() });
+        $("#f__save").click(function() { fileManager.saveFile() });
     });
 }
