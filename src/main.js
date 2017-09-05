22a2a97ecbcc1a53a8ccfa268188c51c79097ca3
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-05 11:51:20 +0200

    Removed unused code

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
 const { ipcRenderer, remote } = require("electron");
+const { app, BrowserWindow }  = require("electron");
 const fs = require("fs");
-const FileManager = require("./api/fileManager");
 
 window.onload = () => {
-    /*loader().then((monaco) => {
-        let editor = monaco.editor.create(document.getElementById("container"), {
-            language: "javascript",
-            theme: "vs-dark",
-            cursorBlinking: "smooth",
-            cursorStyle: "line"
-        });
 
-        let fileManager = new FileManager({editor});
-
-        ipcRenderer.on("navigate", (e, url) => {
-            url = url.slice(8);
-            fs.readFile(url, "utf8", (error, result) => { if(!error) editor.setModel(monaco.editor.createModel(result, "javascript")); });
-        });
-
-        $("#f__open").click(function() { fileManager.openFile() });
-        $("#f__save").click(function() { fileManager.saveFile() });
-    });*/
 }
