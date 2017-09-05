4dbb3dc59e525598972bab51c6b44ab61cb673cc
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-05 12:33:42 +0200

    Allow window to be resized

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-const { ipcRenderer, remote } = require("electron");
+const { ipcRenderer } = require("electron");
+const remote = require("electron").remote;
 const { app, BrowserWindow }  = require("electron");
 const fs = require("fs");
 
 window.onload = () => {
-
+    var win = remote.getCurrentWindow();
+    win.setResizable(true);
 }
