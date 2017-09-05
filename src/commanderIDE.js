1a2f4ac69f016c15666814bda3dd85174bb7835e
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-05 11:04:59 +0200

    Added new editor

---
+++ src/commanderIDE.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const electron = require("electron");
+const path     = require("path");
+const fs       = require("fs");
+
+require('app-module-path').addPath(path.resolve(electron.remote.app.getAppPath(), 'src'));
+
+const customElements = require("./customElements");
+const tabHandle      = require("./api/TabFolder");
+const dashHandle     = require("./api/Dashboard");
+class CommanderIDE extends tabHandle.default {
+    constructor() {
+        super();
+        this.loadExtensions();
+    }
+    loadExtensions() {
+        this.extensions = {};
+        const extDir = path.resolve(electron.remote.app.getAppPath(), "src/api/extensions");
+        fs.readdirSync(extDir).map(dir => {
+            const extension = require("./api/extensions/CodeUI").default;
+            this.extensions["commander-code"] = extension;
+        });
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        const codeProvider = this.extensions['commander-code'].pageProviders['code-page'];
+        const codePage = codeProvider.create();
+        codePage.setAttribute(tabHandle.default.attributeLabel, codeProvider.label);
+        codePage.classList.add(tabHandle.default.classActive);
+        this.appendChild(codePage);
+        const dashboard = new dashHandle.default();
+        dashboard.setAttribute(tabHandle.default.attributeLabel, 'Settings');
+        this.appendChild(dashboard);
+    }
+}
+CommanderIDE.tag = 'commander-ide';
+exports.default = CommanderIDE;
+customElements.default.define(CommanderIDE.tag, CommanderIDE);
